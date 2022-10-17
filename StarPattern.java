a948890db2f1afa9e7667a56618eb59e5f3e8ab9
rushabhthakare <rushabh.thakare10@gmail.com>
2022-10-17 10:42:36 +0530

    My Project

---
+++ StarPattern.java
@@ -0,0 +1,27 @@
+package conditional_statement;
+
+public class StarPattern {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++)
+                if (i % 2 == 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(j+ " ");
+                }
+            System.out.println();
+        }
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 4; j >= i; j--)
+                if (i % 2 == 0) {
+                    System.out.print("* ");
+                }
+            else
+                {
+                    System.out.print(j+ " ");
+                }
+            System.out.println();
+        }
+    }
+}
